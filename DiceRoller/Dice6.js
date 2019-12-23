20cfec67adb442196054c7b5ae2ab02f20d6bca2
Miika Saajanne <miikasaajanne@gmail.com>
2019-12-23 13:30:34 +0200

    Kansion päivitys

--- DiceRoller/Dice6.js
+++ DiceRoller/Dice6.js
@@ -1,20 +1,20 @@
 //Copyright (c) 2019 by Jacob Schaefer (https://codepen.io/Pyremell/pen/eZGGXX)
-  //Dice6
+    //Dice6
 var dice6 = {
     sides: 6,
     roll6: function () {
       var randomNumber = Math.floor(Math.random() * this.sides) + 1;
       return randomNumber;
     }
-  }
+}
   //Prints dice roll to the page
-  function printNumber(number6) {
+function printNumber(number6) {
     var placeholder6 = document.getElementById('placeholder6');
     placeholder6.innerHTML = number6;
-  }
+}
 
-  var button6 = document.getElementById('button6');
-  button6.onclick = function() {
+var button6 = document.getElementById('button6');
+button6.onclick = function() {
     var result6 = dice6.roll6();
     printNumber(result6);
-  };//Dice6
+};  //Dice6
